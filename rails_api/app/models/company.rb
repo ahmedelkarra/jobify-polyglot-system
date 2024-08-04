ed598a901567755251dfd186af73ff28878cd8f4
ahmedelkarra <ahmedelkarra@gmail.com>
2024-08-04 12:56:58 +0200

    done make validations for the register model

--- rails_api/app/models/company.rb
+++ rails_api/app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ApplicationRecord
+  validates :owner_first_name, presence: true
+  validates :owner_last_name, presence: true
+  validates :email, presence: true , uniqueness: true
+  validates :username, presence: true , uniqueness: true
+  validates :company_name, presence: true , uniqueness: true
+  validates :password, presence: true , uniqueness: true , length: {minimum: 7}
 end
