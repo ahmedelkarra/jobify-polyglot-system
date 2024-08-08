857343169cffddb3a6f4121b62548e2d5ec62b4c
ahmedelkarra <ahmedelkarra@gmail.com>
2024-08-08 16:41:12 +0200

    return token after registration to login automatic after registered

--- rails_api/app/controllers/register_controller.rb
+++ rails_api/app/controllers/register_controller.rb
@@ -1,5 +1,7 @@
 require 'bcrypt'
+require 'jwt'
 class RegisterController < ApplicationController
+  HMAC_SECRET = ENV['HMAC_SECRET']
   def register_form
     owner_first_name = params[:owner_first_name]
     owner_last_name = params[:owner_last_name]
@@ -22,7 +24,18 @@ class RegisterController < ApplicationController
     )
     
     if company.save
-      render json: { message: 'Company has been created' }, status: :created
+      payload = {
+        id: company.id,
+        owner_first_name: company.owner_first_name,
+        owner_last_name: company.owner_last_name,
+        company_name: company.company_name,
+        email: company.email,
+        username: company.username,
+        website: company.website,
+        exp: (Time.now + 1.week).to_i
+      }
+      token = JWT.encode(payload, HMAC_SECRET, 'HS256')
+      render json: { message: 'Company has been created' , token: token}, status: :created
     else
       render json: { message: company.errors.full_messages[0] }, status: :unprocessable_entity
     end
